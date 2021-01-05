030c26487e353279af52a1c0ea5d10f97c0e64ce
李玺 <506585803@qq.com>
2021-01-06 00:26:09 +0800

    提交登录页面

---
+++ src/components/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login-wrapper">
+    登陆
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped lang="less">
+.login-wrapper {
+  background-color: #274A6C;
+}
+</style>
