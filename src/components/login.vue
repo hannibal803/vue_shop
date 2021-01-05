7ecb065ffeed3dd98c3d581d09d7f3d203243bc2
李玺 <506585803@qq.com>
2021-01-06 00:51:48 +0800

    登陆方法

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="login-wrapper">
-    登陆
+    <el-form ref="form" :model="form" label-width="80px" class="login-form">
+      <el-form-item label="用户名">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登陆</el-button>
+        <el-button type="info" @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
 export default {
-  name: '',
-  data () {
-    return {}
+  name: "",
+  data() {
+    return {
+      form: {
+        name: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+      login() {
+        console.log(this.form.name);
+        console.log(this.form.password);
+      },
+      reset() {
+        this.form.name = ''
+        this.form.password = ''
+      }
   }
-}
+};
 </script>
 <style scoped lang="less">
 .login-wrapper {
-  background-color: #274A6C;
+  width: 100%;
+  height: 100%;
+  background-color: #274a6c;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .login-form {
+    width: 600px;
+    height: 400px;
+    padding: 20px;
+    background-color: #fff;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式
 import './assets/css/global.css'
+// 全局引入element
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
